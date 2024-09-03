1f9a4d345c891103a21c72870ef8583a55d03a29
Gonzakh <gonzaaa.rag@gmail.com>
2024-09-03 16:24:54 -0300

    Incorporacion de verificacion de Personas incluidas (EN REVISION)

--- Program.cs
+++ Program.cs
@@ -8,17 +8,22 @@ namespace Proyectos
 {
     class Programa_de_Facturas
     {
+        static int HsT; //Variable de la cantidad de horas totales.
+        static int PxP; //Variable del precio de la aventura por persona. 
+        static int PerInc; //Variable de la cantidad de personas incluidas.
+        static string AdvType; // Variable de tipo de aventura.
+        static int NPaquete; // Variable del número de paquete.
         static int PerToAv = 0; //Variable registro de la cantidad de personas totales que hicieron aventuras.
         static int MinHs = 0; //Variable donde se almacena la cantidad de horas de la aventura con menos horas incurridas.
-        static string AvcMinhs; //Variable donde se almacena la aventura con menos horas incurridas.
+        static string AdvConMinHsT; //Variable donde se almacena la aventura con menos horas incurridas.
         static void DataRec() //Clase donde se ingresan los datos de la factura.
 
         {
 
-            int NPaquete; //Ingreso de Número del paquete y validacion del dato.
+             //Ingreso de Número del paquete y validacion del dato.
             while (true)
             {
-
+                
                 Console.WriteLine("Ingrese él Número del paquete:");
 
                 string Ingreso = Console.ReadLine();
@@ -31,11 +36,16 @@ namespace Proyectos
                 {
                     Console.WriteLine("Entrada inválida, ingrese un número.");
                 }
-
             }
 
-            int PerInc; //Ingreso del Número de Personas Incluidas.
-            while (true)
+            if (NPaquete == 0) //Cierre del programa al ingresar 0.
+            {
+                Console.WriteLine("Ingresos finalizados.");
+                Console.ReadLine();
+                return;
+            }
+
+            while (true) //Ingreso del Número de Personas Incluidas.
             {
 
                 Console.WriteLine("Ingrese la Cantidad de personas Incluidas:");
@@ -51,9 +61,12 @@ namespace Proyectos
                     Console.WriteLine("Entrada inválida, ingrese un número.");
                 }
             }
-
-            int PxP; //Ingreso del Número de precio por persona.
-            while (true)
+            if (PerInc == 0) //Verificación de venta de paquete de aventura. 
+            {
+                Console.WriteLine("Se ha ingresado una aventura sin ventas. Por favor ingrese una aventura que se haya vendido al público.");
+                return;
+            }
+                while (true) //Ingreso del Número de precio por persona.
             {
 
                 Console.WriteLine("Ingrese el Precio por Persona:");
@@ -70,8 +83,8 @@ namespace Proyectos
                 }
             }
 
-            int HsT; //Ingreso del Número de Horas Totales.
-            while (true)
+            
+            while (true)//Ingreso del Número de Horas Totales.
             {
                 Console.WriteLine("Ingrese las Horas totales registradas:");
 
@@ -87,8 +100,6 @@ namespace Proyectos
                 }
             }
 
-            string AdvType; // Variable de
-
             while (true) //Entrada y validación de tipo de aventura. 
             {
                 Console.WriteLine("Ingrese una aventura (M, T, R, B, C, E, K, S, J, P):");
@@ -101,52 +112,52 @@ namespace Proyectos
                         case "M":
                             Console.WriteLine("Montaña");
                             Console.Clear();
-                            AvcMinhs = "Montaña";
+                            
                             break;
                         case "T":
                             Console.WriteLine("Trekking");
                             Console.Clear();
-                            AvcMinhs = "Trekking";
+                            
                             break;
                         case "R":
                             Console.WriteLine("Rafting");
                             Console.Clear();
-                            AvcMinhs = "Rafting";
+                            
                             break;
                         case "B":
                             Console.WriteLine("Bicicleta");
                             Console.Clear();
-                            AvcMinhs = "Bicicleta";
+                            
                             break;
                         case "C":
                             Console.WriteLine("Canopy");
                             Console.Clear();
-                            AvcMinhs = "Canopy";
+                            
                             break;
                         case "E":
                             Console.WriteLine("Escalar");
                             Console.Clear();
-                            AvcMinhs = "Escalar";
+                            
                             break;
                         case "K":
                             Console.WriteLine("Sky");
                             Console.Clear();
-                            AvcMinhs = "Sky";
+                            
                             break;
                         case "S":
                             Console.WriteLine("Snowboard");
                             Console.Clear();
-                            AvcMinhs = "Snowboard";
+                            
                             break;
                         case "J":
                             Console.WriteLine("Jumping");
                             Console.Clear();
-                            AvcMinhs = "Jumping";
+                           
                             break;
                         case "P":
                             Console.WriteLine("Parapente");
                             Console.Clear();
-                            AvcMinhs = "Parapente";
+                           
                             break;
                         default:
                             Console.WriteLine("Ingrese una aventura válida");
@@ -160,17 +171,51 @@ namespace Proyectos
 
             }
         }
-        static void ProcesamientoDeDatos() 
+
+        static void ProcesamientoDeDatos()
         {
             while (NPaquete != 0)
-            { }
-        }
+            {
+                String TipoAdvActual = AdvType;
+                int MaxVenta = 0;
+                int PaquetesVendidos = 0;
+
+                void MostrarMaxYPaquetesV()
+                {
+                    Console.WriteLine("Se vendieron un total de: " + PaquetesVendidos + " Paquetes la anterior aventura.");
+                    Console.WriteLine("La venta que más recaudó fue de: $" + MaxVenta);
+                    Console.ReadLine();
+                }
+
+                while (TipoAdvActual == AdvType) 
+                {
+                        PaquetesVendidos++;
+                        PerToAv =+ PerInc; //PROBLEMA
+                        int TotalRecaudado = PxP * PerInc;
+                        if (TotalRecaudado > MaxVenta)
+                        {
+                            MaxVenta = TotalRecaudado;
+                        }
+                        if (MinHs < HsT) //Horas Minimas.PROBLEMA
+                        {
+                            MinHs = HsT;
+                            AdvConMinHsT = AdvType;
+                        }
+                        DataRec();
+                 }
+                MostrarMaxYPaquetesV();
+            }
+        }       
         static void MostrarResultadosMinimosAv()
         {
-            Console.WriteLine("La Aventura con menos horas incurridas fue: " + AvcMinhs); //Aventura con menos Horas incurridas.
+            Console.WriteLine("La Aventura con menos horas incurridas fue: " + AdvConMinHsT); //Aventura con menos Horas incurridas.
             Console.WriteLine("La cantidad de horas incurridas fueron de: " + MinHs); //Cantidad de Horas de la aventura con menos horas incurridas.
         }
-        
+        static void MostrarPersonasTo() //Funcion de personas totales.
+        {
+            Console.WriteLine(PerToAv + " Disfrutaron de Aventuras esta temporada.");
+            
+        }
         static void Main(string[] args) //Programa principal.
         {
             Console.WriteLine("Presione la tecla 'ENTER' para Ingresar datos del paquete");
@@ -179,7 +224,9 @@ namespace Proyectos
             DataRec();//Funcion de ingreso de datos.
             ProcesamientoDeDatos();
             MostrarResultadosMinimosAv();//Funcion de Mínimo de Horas en una Aventura.
-                
+            MostrarPersonasTo(); //Funcion de personas totales.
+            
+            
             Console.ReadKey();
         }
     }
